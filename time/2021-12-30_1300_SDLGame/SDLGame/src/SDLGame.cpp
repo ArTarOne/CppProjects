847dad30f4a69af69c8953d87a06d1014bf5e6d6
Sergey Tyulenev <marleeeeeey@gmail.com>
2021-12-30 21:41:57 +0300

    extract free init and render functions

--- time/2021-12-30_1300_SDLGame/SDLGame/src/SDLGame.cpp
+++ time/2021-12-30_1300_SDLGame/SDLGame/src/SDLGame.cpp
@@ -2,14 +2,16 @@
 
 SDL_Window*   g_pWindow   = nullptr;
 SDL_Renderer* g_pRenderer = nullptr;
+bool          g_bRunning  = false;
 
-int main(int /*argc*/, char* /*args*/[])
+bool init(const char* title, int xpos, int ypos, int height, int width,
+          int         flags)
 {
     // initialize SDL
     if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
     {
         // if succeeded create our window
-        g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+        g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
         // if the window creation succeeded create our renderer
         if(g_pWindow != nullptr)
         {
@@ -18,8 +20,13 @@ int main(int /*argc*/, char* /*args*/[])
     }
     else
     {
-        return 1; // sdl could not initialize
+        return false; // sdl could not initialize
     }
+    return true;
+}
+
+void render()
+{
     // everything succeeded lets draw the window
     // set to black // This function expects Red, Green, Blue and
     // Alpha as color values
@@ -29,8 +36,25 @@ int main(int /*argc*/, char* /*args*/[])
     // show the window
     SDL_RenderPresent(g_pRenderer);
     // set a delay before quitting
-    SDL_Delay(5000);
-    // clean up SDL
+}
+
+int main(int /*argc*/, char* /*args*/[])
+{
+    if(init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1;
+    }
+
+    while(g_bRunning)
+    {
+        render();
+    }
+
     SDL_Quit();
     return 0;
 }
